a0032d185b3b4cdea2c1fed9ffbd5d555f401342
S0FR0 <sofroneacarloss@gmail.com>
2025-01-09 15:51:38 +0200

    Changed object factory to class

--- script.js
+++ script.js
@@ -1,11 +1,77 @@
 const myLibrary = [];
 
-function Books(title, author, pageNumber, status, id) {
-    this.title = title;
-    this.author = author;
-    this.pageNumber = pageNumber;
-    this.status = status;
-    this.id = id;
+class Books{
+    constructor(title, author, pageNumber, status, id){
+        this.title = title;
+        this.author = author;
+        this.pageNumber = pageNumber;
+        this.status = status;
+        this.id = id;
+    }
+
+    addBookToLibrary(book) {myLibrary.push(book);}
+
+    createContainer(title, author, pageNumber, status, id){
+    
+        const box = document.createElement('div');
+        box.setAttribute('class', `box ${id}`)
+        box.setAttribute('id', `${id}`)
+        
+        const boxTitle = document.createElement('h3');
+        boxTitle.setAttribute('class', 'title');
+        boxTitle.textContent = `${title}`;
+        
+        const boxAuthor = document.createElement('h4');
+        boxAuthor.setAttribute('class', 'author');
+        boxAuthor.textContent = `${author}`;
+        
+        const boxPageNumber = document.createElement('h4');
+        boxPageNumber.setAttribute('class', 'pageNumber');
+        boxPageNumber.textContent = `${pageNumber}`;
+    
+        const statusbtn = document.createElement('button');
+        statusbtn.textContent = `${status}`;
+    
+        if(status === 'read'){
+            statusbtn.style.cssText = 'background-color: red';
+        }
+    
+        else{
+            statusbtn.style.cssText = 'background-color: green';
+        }
+    
+        statusbtn.addEventListener('click', () =>{
+            if(statusbtn.textContent === 'read'){
+                statusbtn.textContent = 'not red'
+                statusbtn.style.cssText = 'background-color: green';
+            }
+            else{
+                statusbtn.textContent = 'read'
+                statusbtn.style.cssText = 'background-color: red';
+            }
+        })
+    
+        const button = document.createElement('button');
+        button.setAttribute('class', 'delete');
+        button.setAttribute('id', `${id}`);
+        button.textContent = 'Delete';
+        button.addEventListener('click', () => {
+            let buttonId = (button.getAttribute('id'))
+            const deleteBox = document.getElementById(`${buttonId}`)
+                deleteBox.remove();
+                for( let i = 0; i < myLibrary.length; i++){
+                if(myLibrary[i].id == buttonId)
+                    myLibrary.splice(i, 1);
+            }
+            })
+        
+        box.append(boxTitle);
+        box.append(boxAuthor);
+        box.append(boxPageNumber);
+        box.appendChild(statusbtn);
+        box.append(button);
+        container.appendChild(box);
+    }
 }
 
 function makeCounter() {
@@ -17,73 +83,7 @@ function makeCounter() {
 
 let id = makeCounter();
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-}
-
 const container = document.querySelector('.container');
-
-function createContainer(title, author, pageNumber, status, id){
-    
-    const box = document.createElement('div');
-    box.setAttribute('class', `box ${id}`)
-    box.setAttribute('id', `${id}`)
-    
-    const boxTitle = document.createElement('h3');
-    boxTitle.setAttribute('class', 'title');
-    boxTitle.textContent = `${title}`;
-    
-    const boxAuthor = document.createElement('h4');
-    boxAuthor.setAttribute('class', 'author');
-    boxAuthor.textContent = `${author}`;
-    
-    const boxPageNumber = document.createElement('h4');
-    boxPageNumber.setAttribute('class', 'pageNumber');
-    boxPageNumber.textContent = `${pageNumber}`;
-
-    const statusbtn = document.createElement('button');
-    statusbtn.textContent = `${status}`;
-
-    if(status === 'read'){
-        statusbtn.style.cssText = 'background-color: red';
-    }
-
-    else{
-        statusbtn.style.cssText = 'background-color: green';
-    }
-
-    statusbtn.addEventListener('click', () =>{
-        if(statusbtn.textContent === 'read'){
-            statusbtn.textContent = 'not red'
-            statusbtn.style.cssText = 'background-color: green';
-        }
-        else{
-            statusbtn.textContent = 'read'
-            statusbtn.style.cssText = 'background-color: red';
-        }
-    })
-
-    const button = document.createElement('button');
-    button.setAttribute('class', 'delete');
-    button.setAttribute('id', `${id}`);
-    button.textContent = 'Delete';
-    button.addEventListener('click', () => {
-        let buttonId = (button.getAttribute('id'))
-        const deleteBox = document.getElementById(`${buttonId}`)
-            deleteBox.remove();
-            for( let i = 0; i < myLibrary.length; i++){
-            if(myLibrary[i].id == buttonId)
-                myLibrary.splice(i, 1);
-        }
-        })
-    
-    box.append(boxTitle);
-    box.append(boxAuthor);
-    box.append(boxPageNumber);
-    box.appendChild(statusbtn);
-    box.append(button);
-    container.appendChild(box);
-}
 
 const dialog = document.querySelector('dialog');
 const add = document.querySelector('.add');
@@ -100,7 +100,7 @@ submit.addEventListener('click', (e) => {
     const formStatus = document.querySelector('#status').value;
 
     const newBook = new Books(formTitle, formAuthor, formPages, formStatus, id())
-    addBookToLibrary(newBook);
-    createContainer(newBook.title, newBook.author, newBook.pageNumber, newBook.status, newBook.id);
+    newBook.addBookToLibrary(newBook);
+    newBook.createContainer(newBook.title, newBook.author, newBook.pageNumber, newBook.status, newBook.id);
     dialog.close();
 });
